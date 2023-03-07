218bbd80a576a72187545785512ab64858e795e5
pablossantoss <81624466+pablossantoss@users.noreply.github.com>
2023-03-07 11:28:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ streamlit.title("My Parents New Healthy Diner")
 
 
 streamlit.header('BREAKFAST FAVORITES')
-streamlit.text('🫐Omega 3 & Blueberry Oatmeal')
-streamlit.text('🧋Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍓Omega 3 & Blueberry Oatmeal')
+streamlit.text('🌱Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚Hard-Boiled Free-Range Egg')
 streamlit.text('🥑Avocado Toast')
 
